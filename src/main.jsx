af09f03a733d21d5aaf92d1a3d7ebbd586da966d
Josh Wise <jwise@FuturesLabs-MacBook-Pro.local>
2024-11-13 23:28:09 -0500

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Authenticator } from "@aws-amplify/ui-react";
+import { FileUploader } from "@aws-amplify/ui-react-storage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
